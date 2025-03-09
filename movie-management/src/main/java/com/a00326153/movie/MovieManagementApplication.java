709a844ad1f49fe4d9eef802c0f5cfc5152d11ae
PhilipHerweling24 <147605711+PhilipHerweling24@users.noreply.github.com>
2025-03-09 15:22:03 +0000

    Add files via upload

---
+++ movie-management/src/main/java/com/a00326153/movie/MovieManagementApplication.java
@@ -0,0 +1,13 @@
+package com.a00326153.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementApplication.class, args);
+	}
+
+}
