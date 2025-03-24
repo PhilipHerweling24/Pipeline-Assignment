76058e526480bf71e4282c78ad2c74d27aec9dd2
PhilipHerweling <miggymorette@gmail.com>
2025-03-24 18:20:11 +0000

    Got rid of two unused imports

--- movie-management/src/main/java/com/a00326153/movie/entity/Movie.java
+++ movie-management/src/main/java/com/a00326153/movie/entity/Movie.java
@@ -1,8 +1,6 @@
 package com.a00326153.movie.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
