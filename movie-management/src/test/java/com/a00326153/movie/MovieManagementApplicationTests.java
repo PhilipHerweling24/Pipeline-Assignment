47488c8e07d245d89ba3c9deca657651a94b26e3
PhilipHerweling <miggymorette@gmail.com>
2025-03-26 19:41:15 +0000

    Updated my Jenkins file to hopefully point to my karate tests

--- movie-management/src/test/java/com/a00326153/movie/MovieManagementApplicationTests.java
+++ movie-management/src/test/java/com/a00326153/movie/MovieManagementApplicationTests.java
@@ -1,12 +1,13 @@
 package com.a00326153.movie;
 
+import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieManagementApplicationTests {
 
-	@Test
+	@Karate.Test
 	void contextLoads() {
 		//If the context fails to load, this test will fail automatically.
 	}
